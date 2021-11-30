93db120764966b49158529df1fd1bc166735dfdc
JahnoelRondon <jahnoelrondon03@gmail.com>
2021-11-30 18:49:51 -0500

    fixed base html

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Home.as_view(), name='home')
+    path('', views.Home.as_view(), name='home'),
+    
 ]
 
--- main_app/views.py
+++ main_app/views.py
@@ -4,3 +4,5 @@ from django.contrib.auth.views import LoginView
 # Create your views here.
 class Home(LoginView):
     template_name = 'home.html'
+
+
