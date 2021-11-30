e57a92a8872e7d21ffae21c9665a8cba2de08ad1
lrmoon <larariad@rocketmail.com>
2021-11-30 14:14:39 -0500

    init commit

--- marq/settings.py
+++ marq/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -126,3 +127,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
+
+django_heroku.settings(locals())
+WSGI_APPLICATION = 'marq.wsgi.application'
+DEBUG_PROPAGATE_EXCEPTIONS = True
