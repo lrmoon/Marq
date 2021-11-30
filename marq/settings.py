887cabacbbce1bc18ccb44659abbb38a4b908a23
lrmoon <larariad@rocketmail.com>
2021-11-30 13:45:10 -0500

    init commit

--- marq/settings.py
+++ marq/settings.py
@@ -1,3 +1,6 @@
+import environ
+env = environ.Env()
+environ.Env.read_env()
 """
 Django settings for marq project.
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=9(8d4v-y1n41lebdyct1!iodd-2an(c-rpo384ez*=mdp#^$8'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
