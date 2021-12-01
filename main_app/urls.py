6664dd054040ba5c2e2f2a5811a54654dcf7f989
lrmoon <larariad@rocketmail.com>
2021-12-01 16:23:00 -0500

    authentication

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('todos/', views.todos_index, name='todos_index'),
     path("todos/create/", views.TodoCreate.as_view(), name="todos_create"),
+    path('accounts/signup/', views.signup, name='signup'),
 ]
 
