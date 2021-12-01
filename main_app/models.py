2a337eb2e3bcba747cc25da531f7576b6e4dd4e7
JahnoelRondon <jahnoelrondon03@gmail.com>
2021-12-01 10:12:09 -0500

    added Todo model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from datetime import date
+
+IMPORTANCE = (
+  ('I', 'Important'),
+  ('G', 'General'),
+  ('M', 'Minor')
+)
 
 # Create your models here.
+class Todo(models.Model):
+  title = models.CharField(max_length=10)
+  description = models.CharField(max_length=400)
+
+  date = models.DateField()
+  importance = models.CharField(
+      max_length=1,
+      choices=IMPORTANCE,
+      default=IMPORTANCE[0][1]
+  )
