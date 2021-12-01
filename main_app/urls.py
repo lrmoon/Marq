3bb903ab28ac9ab4727faa5c5de75f9177ad5271
abdelmanaf <ourotoumanaf6@gmail.com>
2021-12-01 14:34:11 -0500

    done todo create form

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
-    path('todos/', views.todos_index, name='todos_index')
+    path('todos/', views.todos_index, name='todos_index'),
+    path("todos/create/", views.TodoCreate.as_view(), name="todos_create"),
 ]
 
