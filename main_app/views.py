7cae7fd22634526b7a47cc8b74f36b91f31f69cd
JahnoelRondon <jahnoelrondon03@gmail.com>
2021-12-01 10:35:03 -0500

    add choices

--- main_app/views.py
+++ main_app/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView
-
-
-
-class Todo:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, title, desc, date, importance):
-    self.title = title
-    self.desc = desc
-    self.date = date
-    self.importance = importance
-
-todos = [
-  Todo('gym', 'go to gym', '01/04/2022.', "important"),
-  Todo('swim', 'go to swimming', '01/07/2022', 'general'),
-  Todo('band', 'go to band', '01/02/2042', 'minor'),
-  Todo('Bank', 'go to  bank', '01/09/2022', 'minor')
-]
+from .models import Todo
 
 
 def todos_index(request):
+    todos = Todo.objects.all()
     return render(request, 'todos/index.html', {"todos": todos})
 
 
