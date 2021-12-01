08cbe466f9bce7d8010e0d2b5bbc251ea2284b82
abdelmanaf <ourotoumanaf6@gmail.com>
2021-12-01 15:08:31 -0500

    add delete url

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('todos/', views.todos_index, name='todos_index'),
     path("todos/create/", views.TodoCreate.as_view(), name="todos_create"),
+    path('todos/<int:pk>/delete/', views.TodosDelete.as_view(), name='todos_delete'),
 ]
 
