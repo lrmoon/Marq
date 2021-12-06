795ee875aecd37a4f95409fb662e4674e7d103a7
Lara Riad <90151829+lrmoon@users.noreply.github.com>
2021-12-06 12:43:48 -0500

    Revert "Aws"

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls import url
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('todos/', views.todos_index, name='todos_index'),
-    path('todos/create/', views.TodoCreate.as_view(), name="todos_create"),
+    path("todos/create/", views.TodoCreate.as_view(), name="todos_create"),
     path('todos/<int:pk>/update/', views.TodoUpdate.as_view(), name='todos_update'),
     path('todos/<int:pk>/delete/', views.TodoDelete.as_view(), name='todos_delete'),
     path('accounts/signup/', views.signup, name='signup'),
@@ -15,12 +15,5 @@ urlpatterns = [
     path('events/create/', views.event, name='event_new'),
     path('events/<int:pk>/update/', views.EventUpdate.as_view(), name='event_edit'),
     path('events/<int:pk>/delete/', views.EventDelete.as_view(), name='events_delete'),
-   
-
-    path('notes/', views.notes_index, name='notes_index'),
-    path('notes/create/', views.NoteCreate.as_view(), name="notes_create"),
-    path('notes/<int:pk>/update/', views.NoteUpdate.as_view(), name='notes_update'),
-    path('notes/<int:pk>/delete/', views.NoteDelete.as_view(), name='notes_delete'),
-    path('notes/<int:note_id>/add_photo/', views.add_photo, name='add_photo'),
 ]
 
